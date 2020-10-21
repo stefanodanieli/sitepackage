2c32fa2ea6549f65570b46279375d534e9aa3824
stefano <stefano.danieli@polimi.it>
2020-10-21 08:21:48 +0200

    new message

---
+++ Resources/Public/JavaScript/Dist/scripts.js
@@ -0,0 +1,6 @@
+/*!
+ * Sitepackage v1.0.0 (https://www.polimi.it)
+ * Copyright 2017-2020 Stefano Danieli
+ * Licensed under the GPL-2.0-or-later license
+ */
+console.log("WE LOVE TYPO3");
---
+++ ext_emconf.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Extension Manager/Repository config file for ext "sitepackage".
+ */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Sitepackage',
+    'description' => '',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'bootstrap_package' => '11.0.0-11.0.99',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'PolitecnicoDiMilano\\Sitepackage\\' => 'Classes',
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Stefano Danieli',
+    'author_email' => 'stefano.danieli@polimi.it',
+    'author_company' => 'Politecnico di Milano',
+    'version' => '1.0.0',
+];
